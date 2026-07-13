195e3270f699e30786692fd8aaf16f480eb1f66a
yousifaadwkf-droid <omarhabahba0@gmail.com>
2026-07-13 03:01:25 +0300

    Fix TOKEN

--- config.py
+++ config.py
@@ -4,9 +4,14 @@ TOKEN = os.getenv("8389405270:AAHYTA46DfY-OsFV4LoFgDpf-tZ32jOeEdc")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "606221907"))
 
-ADMINS = list(map(int, os.getenv(
-    "ADMINS",
-    "606221907,1343055427,1736876324"
-).split(",")))
+ADMINS = list(
+    map(
+        int,
+        os.getenv(
+            "ADMINS",
+            "606221907,1343055427,1736876324"
+        ).split(",")
+    )
+)
 
 BOT_NAME = os.getenv("BOT_NAME", "مساعد تجي🤍")
